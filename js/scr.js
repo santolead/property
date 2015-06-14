15a9b660fc5ee1ca49055a1796cbdc776ce5603a
santo <santolead.mail@gmail.com>
2015-06-15 00:40:09 +0700

    property
    property theme v.1.0

---
+++ js/scr.js
@@ -0,0 +1,15 @@
+// https://github.com/santolead/property
+
+$( document ).ready(function() {
+	$('a.page-scroll').bind('click', function(event) {
+		var $anchor = $(this);
+		$('html, body').stop().animate({
+			scrollTop: $($anchor.attr('href')).offset().top
+		}, 1500, 'easeInOutExpo');
+		event.preventDefault();
+	});
+	
+	$('.carousel').carousel({
+		pause: "false"
+    });
+});
